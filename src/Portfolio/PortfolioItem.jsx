e44e954e1a9c69936cbe7a82ebae7e82cd7184e0
AshishGujral <ashishgujral22@gmail.com>
2023-05-16 01:03:14 -0700

    fix

--- src/Portfolio/PortfolioItem.jsx
+++ src/Portfolio/PortfolioItem.jsx
@@ -9,7 +9,7 @@ function PortfolioItem({ category, imageSrc, linkUrl, title, subtitle }) {
     <div className={`col-md-6 col-lg-4 ${category} new`}>
       <div className="portfolio-item">
         <a href={linkUrl} target="_blank" rel="noopener noreferrer">
-          <img src={imageSrc} className="img-fluid" alt={title} style={{ height: '200px', width: '90%' }} />
+          <img src={imageSrc} className="img-fluid" alt={title} style={{ height: '200px', width: '300px' }} />
         </a>
         <div className="content-holder" onClick={handleClick}>
           <div className="text-holder">
